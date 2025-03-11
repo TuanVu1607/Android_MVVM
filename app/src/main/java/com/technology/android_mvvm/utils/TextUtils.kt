4ca8998bb2b1c21f0e23b7325049fb3b650ec8ce
TuanVu1607 <tuanvutran369@gmail.com>
2025-03-11 12:14:43 +0700

    Update 11/03/2025

--- app/src/main/java/com/technology/android_mvvm/utils/TextUtils.kt
+++ app/src/main/java/com/technology/android_mvvm/utils/TextUtils.kt
@@ -1,6 +1,8 @@
 package com.technology.android_mvvm.utils
 
+import android.graphics.drawable.Drawable
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 
 object TextUtils {
@@ -12,5 +14,105 @@ object TextUtils {
         view.marqueeRepeatLimit = -1 // Repeat indefinitely
         view.maxLines = 1 // Limit 1 lines displayed
         view.isSelected = isMarqueeEnabled // Enable marquee effect
+    }
+
+    @BindingAdapter("drawableStart")
+    @JvmStatic
+    fun setDrawableStart(view: TextView, resourceId: Int) {
+        val drawable = try {
+            ContextCompat.getDrawable(view.context, resourceId)
+        } catch (e: Exception) {
+            null
+        }
+        drawable?.let {
+            it.setBounds(0, 0, it.intrinsicWidth, it.intrinsicHeight)
+            val drawables = view.compoundDrawables
+            view.setCompoundDrawables(it, drawables[1], drawables[2], drawables[3])
+        }
+    }
+
+    @BindingAdapter("drawableStart")
+    @JvmStatic
+    fun setDrawableStart(view: TextView, drawable: Drawable?) {
+        drawable?.let {
+            it.setBounds(0, 0, it.intrinsicWidth, it.intrinsicHeight)
+            val drawables = view.compoundDrawablesRelative
+            view.setCompoundDrawablesRelative(it, drawables[1], drawables[2], drawables[3])
+        }
+    }
+
+    @BindingAdapter("drawableEnd")
+    @JvmStatic
+    fun setDrawableEnd(view: TextView, resourceId: Int) {
+        val drawable = try {
+            ContextCompat.getDrawable(view.context, resourceId)
+        } catch (e: Exception) {
+            null
+        }
+        drawable?.let {
+            it.setBounds(0, 0, it.intrinsicWidth, it.intrinsicHeight)
+            val drawables = view.compoundDrawables
+            view.setCompoundDrawables(drawables[0], drawables[1], it, drawables[3])
+        }
+    }
+
+    @BindingAdapter("drawableEnd")
+    @JvmStatic
+    fun setDrawableEnd(view: TextView, drawable: Drawable?) {
+        drawable?.let {
+            it.setBounds(0, 0, it.intrinsicWidth, it.intrinsicHeight)
+            val drawables = view.compoundDrawablesRelative
+            view.setCompoundDrawables(drawables[0], drawables[1], it, drawables[3])
+        }
+    }
+
+    @BindingAdapter("drawableTop")
+    @JvmStatic
+    fun setDrawableTop(view: TextView, resourceId: Int) {
+        val drawable = try {
+            ContextCompat.getDrawable(view.context, resourceId)
+        } catch (e: Exception) {
+            null
+        }
+        drawable?.let {
+            it.setBounds(0, 0, it.intrinsicWidth, it.intrinsicHeight)
+            val drawables = view.compoundDrawables
+            view.setCompoundDrawables(drawables[0], it, drawables[2], drawables[3])
+        }
+    }
+
+    @BindingAdapter("drawableTop")
+    @JvmStatic
+    fun setDrawableTop(view: TextView, drawable: Drawable?) {
+        drawable?.let {
+            it.setBounds(0, 0, it.intrinsicWidth, it.intrinsicHeight)
+            val drawables = view.compoundDrawablesRelative
+            view.setCompoundDrawables(drawables[0], it, drawables[2], drawables[3])
+        }
+    }
+
+    @BindingAdapter("drawableBottom")
+    @JvmStatic
+    fun setDrawableBottom(view: TextView, resourceId: Int) {
+        val drawable = try {
+            ContextCompat.getDrawable(view.context, resourceId)
+        } catch (e: Exception) {
+            null
+        }
+        drawable?.let {
+            it.setBounds(0, 0, it.intrinsicWidth, it.intrinsicHeight)
+            val drawables = view.compoundDrawables
+            view.setCompoundDrawables(drawables[0], drawables[1], drawables[2], it)
+        }
+    }
+
+    @BindingAdapter("drawableBottom")
+    @JvmStatic
+    fun setDrawableBottom(view: TextView, drawable: Drawable?) {
+        drawable?.let {
+            it.setBounds(0, 0, it.intrinsicWidth, it.intrinsicHeight)
+            val drawables = view.compoundDrawablesRelative
+            view.setCompoundDrawables(drawables[0], drawables[1], drawables[2], it)
+        }
     }
 }
